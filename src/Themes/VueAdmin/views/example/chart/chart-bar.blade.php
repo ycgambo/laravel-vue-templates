419a7dd981938bee7b523b82bee5b5f093b4212b
ycgambo <17816875364@163.com>
2019-07-15 13:51:53 +0800

    chart-bar and chart-line enchancement

--- src/Themes/VueAdmin/views/example/chart/chart-bar.blade.php
+++ src/Themes/VueAdmin/views/example/chart/chart-bar.blade.php
@@ -6,7 +6,8 @@
         'C0CC' => [220, 182, 125, 145, 122, 191, 34, 50, 120, 110, 165, 122],
         'CUCC' => [220, 182, 125, 15, 122, 191, 134, 150, 120, 110, 165, 122],
     ];
-    $data['ref'] = ["13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55"];
+    $refs = ["13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55"];
+    $legends = ["中文图例", "数据2", "数据3"];
 @endphp
 
 <code-php>{!! <<<'DOC'
@@ -15,33 +16,49 @@
         'C0CC' => [220, 182, 125, 145, 122, 191, 34, 50, 120, 110, 165, 122],
         'CUCC' => [220, 182, 125, 15, 122, 191, 134, 150, 120, 110, 165, 122],
     ];
-    $data['ref'] = ["13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55"];
+    $refs = ["13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55"];
+    $legends = ["中文图例", "数据2", "数据3"];
 DOC;
 ; !!}</code-php>
 
 
-<h1 class="mt-3">Default</h1>
+<h1 class="mt-3">Basic</h1>
 <div class="mt-3">
-    <chart-bar width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis">@json($data)</chart-bar>
+    <chart-bar refs='@json($refs)' legends='@json($legends)' height="500px" title="Title" x-name="xAxis" y-name="yAxis">@json($data)</chart-bar>
 </div>
 
 <b-badge variant="primary" v-b-toggle.default>Source Code</b-badge>
 <b-collapse id="default" class="mt-2"><code-html>@php echo e( <<<'DOC'
-    <chart-bar width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis">@json($data)</chart-bar>
+    <chart-bar refs='@json($refs)' legends='@json($legends)' height="500px" title="Title" x-name="xAxis" y-name="yAxis">@json($data)</chart-bar>
 DOC
 )@endphp</code-html></b-collapse>
 
 
 <h1 class="mt-3">Stack</h1>
 <div class="mt-3">
-    <chart-bar width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" stack>@json($data)</chart-bar>
+    <chart-bar refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" stack>@json($data)</chart-bar>
 </div>
 
 <b-badge variant="primary" v-b-toggle.stack>Source Code</b-badge>
 <b-collapse id="stack" class="mt-2"><code-html>@php echo e( <<<'DOC'
-    <chart-bar width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" stack>@json($data)</chart-bar>
+    <chart-bar refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" stack>@json($data)</chart-bar>
 DOC
 )@endphp</code-html></b-collapse>
+
+
+<h1 class="mt-3">chart-bar Attributes</h1>
+<chart-table>
+    [
+    {"attribute": "refs", "description": "reference on x axis", "type": "string of json array", "options": "—", "default": "—"},
+    {"attribute": "title", "description": "title of chart", "type": "string", "options": "—", "default": "—"},
+    {"attribute": "width", "description": "width of chart", "type": "string", "options": "—", "default": "100%"},
+    {"attribute": "height", "description": "height of chart", "type": "string", "options": "—", "default": "200px"},
+    {"attribute": "x-name", "description": "name of x axis", "type": "string", "options": "—", "default": "—"},
+    {"attribute": "y-name", "description": "name of y axis", "type": "string", "options": "—", "default": "—"},
+    {"attribute": "legends", "description": "legends of chart", "type": "string of json array", "options": "—", "default": "—"},
+    {"attribute": "stack", "description": "stack mode", "type": "boolean", "options": "—", "default": "false"}
+    ]
+</chart-table>
 
 
 @endlvt_example
--- src/Themes/VueAdmin/views/example/chart/chart-line.blade.php
+++ src/Themes/VueAdmin/views/example/chart/chart-line.blade.php
@@ -6,7 +6,8 @@
         'C0CC' => [220, 182, 125, 145, 122, 191, 34, 50, 120, 110, 165, 122],
         'CUCC' => [220, 182, 125, 15, 122, 191, 134, 150, 120, 110, 165, 122],
     ];
-    $data['ref'] = ["13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55"];
+    $refs = ["13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55"];
+    $legends = ["中文图例", "数据2", "数据3"];
 @endphp
 
 <code-php>{!! <<<'DOC'
@@ -15,69 +16,86 @@
         'C0CC' => [220, 182, 125, 145, 122, 191, 34, 50, 120, 110, 165, 122],
         'CUCC' => [220, 182, 125, 15, 122, 191, 134, 150, 120, 110, 165, 122],
     ];
-    $data['ref'] = ["13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55"];
+    $refs = ["13:00", "13:05", "13:10", "13:15", "13:20", "13:25", "13:30", "13:35", "13:40", "13:45", "13:50", "13:55"];
+    $legends = ["中文图例", "数据2", "数据3"];
 DOC;
 ; !!}</code-php>
 
 
-<h1 class="mt-3">Zigzag</h1>
+<h1 class="mt-3">Basic</h1>
 <div class="mt-3">
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis">@json($data)</chart-line>
+    <chart-line refs='@json($refs)' legends='@json($legends)' height="500px" title="Title" x-name="xAxis" y-name="yAxis">@json($data)</chart-line>
 </div>
 
 <b-badge variant="primary" v-b-toggle.zigzag>source code</b-badge>
 <b-collapse id="zigzag" class="mt-2"><code-html>@php echo e( <<<'DOC'
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis">@json($data)</chart-line>
+    <chart-line refs='@json($refs)' legends='@json($legends)' height="500px" title="Title" x-name="xAxis" y-name="yAxis">@json($data)</chart-line>
 DOC
 )@endphp</code-html></b-collapse>
 
 
 <h1 class="mt-3">Smooth</h1>
 <div class="mt-3">
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" smooth>@json($data)</chart-line>
+    <chart-line refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" smooth>@json($data)</chart-line>
 </div>
 
 <b-badge variant="primary" v-b-toggle.smooth>Source Code</b-badge>
 <b-collapse id="smooth" class="mt-2"><code-html>@php echo e( <<<'DOC'
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" smooth>@json($data)</chart-line>
+    <chart-line refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" smooth>@json($data)</chart-line>
 DOC
 )@endphp</code-html></b-collapse>
 
 
 <h1 class="mt-3">Step at Start</h1>
 <div class="mt-3">
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="start">@json($data)</chart-line>
+    <chart-line refs='@json($refs)'height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="start">@json($data)</chart-line>
 </div>
 
 <b-badge variant="primary" v-b-toggle.step>Source Code</b-badge>
 <b-collapse id="step" class="mt-2"><code-html>@php echo e( <<<'DOC'
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="start">@json($data)</chart-line>
+    <chart-line refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="start">@json($data)</chart-line>
 DOC
 )@endphp</code-html></b-collapse>
 
 
 <h1 class="mt-3">Step at Middle</h1>
 <div class="mt-3">
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="middle">@json($data)</chart-line>
+    <chart-line refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="middle">@json($data)</chart-line>
 </div>
 
 <b-badge variant="primary" v-b-toggle.step>Source Code</b-badge>
 <b-collapse id="step" class="mt-2"><code-html>@php echo e( <<<'DOC'
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="middle">@json($data)</chart-line>
+    <chart-line refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="middle">@json($data)</chart-line>
 DOC
 )@endphp</code-html></b-collapse>
 
 
 <h1 class="mt-3">Step at End</h1>
 <div class="mt-3">
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="end">@json($data)</chart-line>
+    <chart-line refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="end">@json($data)</chart-line>
 </div>
 
 <b-badge variant="primary" v-b-toggle.step>Source Code</b-badge>
 <b-collapse id="step" class="mt-2"><code-html>@php echo e( <<<'DOC'
-    <chart-line width="100%" height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="end">@json($data)</chart-line>
+    <chart-line refs='@json($refs)' height="500px" title="Title" x-name="xAxis" y-name="yAxis" step="end">@json($data)</chart-line>
 DOC
 )@endphp</code-html></b-collapse>
+
+
+<h1 class="mt-3">chart-line Attributes</h1>
+<chart-table>
+    [
+    {"attribute": "refs", "description": "reference on x axis", "type": "string of json array", "options": "—", "default": "—"},
+    {"attribute": "title", "description": "title of chart", "type": "string", "options": "—", "default": "—"},
+    {"attribute": "width", "description": "width of chart", "type": "string", "options": "—", "default": "100%"},
+    {"attribute": "height", "description": "height of chart", "type": "string", "options": "—", "default": "200px"},
+    {"attribute": "x-name", "description": "name of x axis", "type": "string", "options": "—", "default": "—"},
+    {"attribute": "y-name", "description": "name of y axis", "type": "string", "options": "—", "default": "—"},
+    {"attribute": "legends", "description": "legends of chart", "type": "string of json array", "options": "—", "default": "—"},
+    {"attribute": "smooth", "description": "smooth mode", "type": "boolean", "options": "—", "default": "false"},
+    {"attribute": "step", "description": "step mode", "type": "string", "options": "start,middle,end", "default": "—"}
+    ]
+</chart-table>
 
 
 @endlvt_example
