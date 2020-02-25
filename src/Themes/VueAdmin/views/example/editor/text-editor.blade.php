6c14e8f8fcbc5900f30fad13cd5f207d0620777a
ycgambo <17816875364@163.com>
2020-02-25 15:28:20 +0800

    fix:autosave

--- src/Themes/VueAdmin/views/example/editor/text-editor.blade.php
+++ src/Themes/VueAdmin/views/example/editor/text-editor.blade.php
@@ -2,13 +2,13 @@
 
 <h1 class="mt-3">Default</h1>
 <div class="mt-3">
-    <text-editor upload_url="/lvt/VueAdmin/example/img_upload"></text-editor>
+    <text-editor autosave="id1" upload_url="/lvt/VueAdmin/example/img_upload"></text-editor>
 </div>
 
 <b-badge class="mt-3" v-b-toggle.default>Source Code</b-badge>
 <b-collapse id="default" class="mt-2">
     <code-html>@php echo e( <<<'TXTDOC'
-    <text-editor autosave="text" upload_url="/lvt/VueAdmin/example/img_upload"></text-editor>
+    <text-editor autosave="id1" upload_url="/lvt/VueAdmin/example/img_upload"></text-editor>
 TXTDOC
 )@endphp</code-html>
 </b-collapse>
@@ -22,7 +22,7 @@ TXTDOC
 <b-badge class="mt-3" v-b-toggle.height>Source Code</b-badge>
 <b-collapse id="height" class="mt-2">
     <code-html>@php echo e( <<<'DOC'
-    <text-editor min_height="300px" max_height="400px"></text-editor>
+    <text-editor upload_url="/lvt/VueAdmin/example/img_upload" min_height="300px" max_height="400px"></text-editor>
 DOC
 )@endphp</code-html>
 </b-collapse>
@@ -32,7 +32,7 @@ DOC
 <p class="mt-3">Restore lost work by selecting Restore last draft from the File menu</p>
 <p class="mt-3">Editors with the same autosave prop will both changes the same saved content</p>
 <div class="mt-3">
-    <text-editor autosave="text" upload_url="/lvt/VueAdmin/example/img_upload">{!! <<<'DOC'
+    <text-editor autosave="id2" upload_url="/lvt/VueAdmin/example/img_upload">{!! <<<'DOC'
         <p align="center">test<font face="arial black,sans-serif">ijjfoaodfajofao<font size="5">asfdasf<strike>asdfasf<sub>asfsaf<i>adgasd<sup>asdfs</sup></i></sub></strike></font></font></p> <hr /> <p align="left"><font face="arial black,sans-serif"><font size="5"><strike><sub><i><sup>adf</sup></i></sub></strike></font></font></p> <table style="border-collapse: collapse; width: 100%;" border="1"> <tbody> <tr> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> </tr> <tr> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> </tr> </tbody> </table> <pre class="language-php"><code>&lt;?php exit(); </code></pre> <blockquote> <p align="left"><b>fasdfasf<i>fsadfdasgasf</i></b>asaasdfasfgasg</p> </blockquote> <hr /> <p align="right">dfafasfdasdfsadf</p>
 DOC;
 ; !!}</text-editor>
@@ -41,10 +41,10 @@ DOC;
 <b-badge class="mt-3" v-b-toggle.autosave>Source Code</b-badge>
 <b-collapse id="autosave" class="mt-2">
     <code-html>@php echo e( <<<'TXTDOC'
-    <text-editor upload_url="/lvt/VueAdmin/example/img_upload">{!! <<<'DOC'
+    <text-editor autosave="id2" upload_url="/lvt/VueAdmin/example/img_upload">{!! <<<'DOC'
         <p align="center">test<font face="arial black,sans-serif">ijjfoaodfajofao<font size="5">asfdasf<strike>asdfasf<sub>asfsaf<i>adgasd<sup>asdfs</sup></i></sub></strike></font></font></p> <hr /> <p align="left"><font face="arial black,sans-serif"><font size="5"><strike><sub><i><sup>adf</sup></i></sub></strike></font></font></p> <table style="border-collapse: collapse; width: 100%;" border="1"> <tbody> <tr> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> </tr> <tr> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> <td style="width: 16.6667%;">&nbsp;</td> </tr> </tbody> </table> <pre class="language-php"><code>&lt;?php exit(); </code></pre> <blockquote> <p align="left"><b>fasdfasf<i>fsadfdasgasf</i></b>asaasdfasfgasg</p> </blockquote> <hr /> <p align="right">dfafasfdasdfsadf</p>
 DOC;
-; !!}</text-editor>
+; !!}</text-editor
 TXTDOC
 )@endphp</code-html>
 </b-collapse>
